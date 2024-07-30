fd739cef11e71a86064200ef1e390c534b386fab
Luis Calle Lazo <callelazodeynarluis@gmail.com>
2024-07-29 21:25:24 -0400

    Background Infinity

---
+++ Assets/Scripts/Controllers/MotorController.cs
@@ -0,0 +1,90 @@
+
+using FastFurios_Game.Objects;
+using UnityEngine;
+
+namespace FastFurios_Game.Controllers
+{
+    public class MotorController : MonoBehaviour
+    {
+        public GameObject containerCallesGo;
+        public GameObject[] containerCallesArray;
+        public float speed;
+        public bool startGame;
+        public bool endGame;
+        int contCalles = 0;
+        int numSelectorCalles;
+
+        public GameObject calleLast;
+        public GameObject calleNew;
+
+        public float sizeCalle;
+        void Start()
+        {
+            StartGame();
+        }
+
+        void StartGame()
+        {
+            containerCallesGo = GameObject.Find("ContainerCalle");
+            speedMotorRoad();
+            SearchCalle();
+        }
+        
+        void createCalle() 
+        {
+            contCalles ++;
+            numSelectorCalles = Random.Range(0, containerCallesArray.Length);
+            var calle = Instantiate(containerCallesArray[numSelectorCalles]);
+            calle.SetActive(true);
+            calle.name = "Calle" + contCalles;
+            calle.transform.parent = gameObject.transform; 
+            positionCalle();
+        }
+        void SearchCalle() 
+        {
+            containerCallesArray = GameObject.FindGameObjectsWithTag("Calle");
+            int cont = 0;
+            foreach(var calle in containerCallesArray)
+            {
+                calle.gameObject.transform.parent = containerCallesGo.transform;
+                calle.gameObject.SetActive(false);
+                calle.gameObject.name = "CalleOFF_" + cont++;
+            }
+
+            createCalle();
+        }
+        void positionCalle()
+        {
+            calleLast = GameObject.Find("Calle" + (contCalles-1));
+            calleNew = GameObject.Find("Calle" + contCalles);
+            
+            measureCalle();
+            calleNew.transform.position = new Vector3(
+                calleLast.transform.position.x, 
+                calleLast.transform.position.y + sizeCalle - 5,
+                0
+            );
+              
+        }
+        
+        void measureCalle()
+        {
+            for(int i = 0; i < calleLast.transform.childCount; i++)
+            {
+                if(calleLast.transform.GetChild(i).gameObject.GetComponent<PartRoad>() != null)
+                {
+                    float sizePart = calleLast.transform.GetChild(i).gameObject.GetComponent<SpriteRenderer>().bounds.size.y;
+                    sizeCalle += sizePart;
+                }
+            }
+        }
+
+        void speedMotorRoad() => speed = 10;
+        void Update()
+        {
+            if(startGame == true && endGame == false)
+                transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Objects/PartRoad.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FastFurios_Game.Objects
+{
+    public class PartRoad : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
